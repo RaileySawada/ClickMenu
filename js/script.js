1a14dce65c686f287eee6f28318fe3c0b80f8462
Railey Dela Peña <131561799+RaileySawada@users.noreply.github.com>
2023-11-03 16:33:09 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger-menu");
+const backdrop = document.querySelector(".backdrop-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    backdrop.classList.toggle("active");
+})
