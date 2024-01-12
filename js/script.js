dc1c6f4e5aeb259517cfbb4fbb789235a0144a5a
Railey Dela Peña <131561799+RaileySawada@users.noreply.github.com>
2024-01-12 19:07:48 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const backdrop = document.querySelector(".backdrop-menu");
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     backdrop.classList.toggle("active");
 })
+
+const dishImage = document.querySelectorAll(".dish-img");
+const dish = document.querySelector(".dish");
+let activeDishImage = null;
+let activeDish = null;
+dishImage.forEach(dishImage => {
+    dishImage.addEventListener("click", () => {
+        if((activeDishImage && activeDishImage !== dishImage) || (activeDish && activeDish !== dish)){
+            activeDishImage.classList.remove("active");
+            activeDish.classList.remove("active");
+        }
+        dishImage.classList.toggle("active");
+        dishImage.closest(".dish").classList.toggle("active");
+        /*activeDishImage = dishImage;*/
+        activeDishImage = dishImage.classList.contains("active") ? dishImage : null;
+        activeDish = dish.classList.contains("active") ? dish : null;
+    });
+});
